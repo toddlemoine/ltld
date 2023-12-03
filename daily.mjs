4d8860337664ffe4e596774ee6d3de9c5e0daf67
Todd Lemoine <todd.lemoine@gmail.com>
2023-12-03 08:39:40 -0800

    Regenerate words, tweaking bag build

--- daily.mjs
+++ daily.mjs
@@ -42,13 +42,21 @@ const morphemeFreq = morphemes.split(' ').reduce((acc, m) => {
     return acc;
 }, {});
 
-function createBagOfLetters(words, size = 35) {
+function createBagOfLetters(words, size = 50) {
     const wordLetters = words.join('').toUpperCase().split('');
-    const mixedLetters = _.shuffle(makeBag(bigBagSize, freq)).slice(
-        0,
-        size - wordLetters.length,
+    let mixedBagSize = size - wordLetters.length;
+    const mixedLetters = generateBag().slice(0, mixedBagSize);
+    // const score = varietyScore(mixedLetters, wordLetters);
+    // console.log({ score, words, mixedLetters });
+    // if (score < minimumVariety) {
+    //     return createBagOfLetters(words, size, minimumVariety, count + 1);
+    // }
+
+    // Include word letters in the first 35 letters
+    const partWithWordLetters = _.shuffle(
+        wordLetters.concat(mixedLetters.slice(0, size - wordLetters.length)),
     );
-    return _.shuffle(wordLetters.concat(mixedLetters));
+    return partWithWordLetters.concat(mixedLetters.slice(wordLetters.length));
 }
 
 function makeBag(bagSize, freq) {
@@ -62,6 +70,53 @@ function makeBag(bagSize, freq) {
     });
 }
 
+// Function that generate a bag of 100 random letters based on their frequency in the English language
+function generateBag() {
+    const freq = {
+        a: 8.2,
+        b: 1.5,
+        c: 2.8,
+        d: 4.3,
+        e: 12.7,
+        f: 2.2,
+        g: 2.0,
+        h: 6.1,
+        i: 7.0,
+        j: 0.2,
+        k: 0.8,
+        l: 4.0,
+        m: 2.4,
+        n: 6.7,
+        o: 7.5,
+        p: 1.9,
+        q: 0.1,
+        r: 6.0,
+        s: 6.3,
+        t: 9.1,
+        u: 2.8,
+        v: 1.0,
+        w: 2.4,
+        x: 0.2,
+        y: 2.0,
+        z: 0.1,
+    };
+
+    const letterArray = [];
+
+    // Calculate the number of occurrences for each letter based on the frequency
+    for (const letter in freq) {
+        const occurrences = Math.min(1, Math.round((freq[letter] / 100) * 100));
+
+        // Add the letter to the array the specified number of times
+        letterArray.push(...Array(occurrences).fill(letter.toUpperCase()));
+    }
+
+    // Shuffle the entire array to randomize the order of letters
+    // letterArray.sort(() => Math.random() - 0.5);
+
+    return _.shuffle(letterArray);
+}
+
 const currentDate = new Date();
 const numOfLetters = 35;
 const numOfMorphemes = 20;
@@ -73,25 +128,45 @@ function dateFilename(date) {
     return `./daily/${date.toISOString().slice(0, 10)}.json`;
 }
 
+function varietyScore(arr1, arr2) {
+    const source = new Set(arr2.map(l => l.toUpperCase()));
+    const results = arr1.reduce(
+        (acc, letter) => {
+            if (source.has(letter.toUpperCase())) {
+                acc.same++;
+            } else {
+                acc.diff++;
+            }
+            return acc;
+        },
+        { same: 0, diff: 0 },
+    );
+    // console.log({ arr1, arr2, results });
+
+    return results.diff === 0 ? 1 : results.same / results.diff;
+}
+
 function parseWordsInRow(row) {
     const words = row.words.toUpperCase().split(' ');
-    const letterBag = createBagOfLetters(words).join('');
+    const letterBag = createBagOfLetters(words, 50, 0.7).join('');
     const morphemeBag = _.shuffle(makeBag(bigBagSize, morphemeFreq))
         .slice(0, numOfMorphemes)
         .join('');
 
     // console.log('');
     // console.log(row.words);
-    // console.log({
-    //     words,
-    //     bag: JSON.stringify([letterBag, morphemeBag].join(' ')),
-    // });
 
     const data = {
         words,
         bag: [letterBag, morphemeBag].join(' '),
     };
     const filename = dateFilename(add(currentDate, { days: rowCount }));
+
+    console.log({
+        filename,
+        ...data,
+    });
+
     fs.writeFileSync(filename, JSON.stringify(data));
     rowCount += 1;
 }
--- score.mjs
+++ score.mjs
@@ -2,10 +2,25 @@
 // based on how many elements they have in common. 1 means they are identical.
 // 0 means they have nothing in common.
 function varietyScore(arr1, arr2) {
-    const arr1Set = new Set(arr1);
-    const arr2Set = new Set(arr2);
-    const intersection = new Set([...arr1Set].filter(x => arr2Set.has(x)));
-    return intersection.size / arr1.length;
+    // const arr1Set = new Set(arr1);
+    // const arr2Set = new Set(arr2);
+    // const intersection = new Set([...arr1Set].filter(x => arr2Set.has(x)));
+    // return intersection.size / arr1.length;
+    const results = arr1.reduce(
+        (acc, letter) => {
+            if (arr2.includes(letter)) {
+                acc.same++;
+            } else {
+                acc.diff++;
+            }
+            return acc;
+        },
+        { same: 0, diff: 0 },
+    );
+    if (results.diff === 0) {
+        return 1;
+    }
+    return results.same / results.diff;
 }
 
 console.log('Expect score 1:');
@@ -16,3 +31,8 @@ console.log(varietyScore(['a', 'b', 'c'], ['d', 'e', 'f'])); // 1
 
 console.log('Expect score in between:');
 console.log(varietyScore('defayunwiapolikujmbc'.split(''), 'abc'.split(''))); // 1
+
+console.log('This score is closer to 1:');
+console.log(
+    varietyScore('dbbbbbefayunbwiapcaobblikujmbc'.split(''), 'abc'.split('')),
+); // 1
